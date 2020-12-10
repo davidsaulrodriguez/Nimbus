5aa25c0b290240ee2d3f24552a4577ce1c8b1a1f
David Rodriguez <david@bsdadm.com>
2020-12-10 12:54:37 -0500

    remove nonsense code to get started with the JS.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,5 @@
 $(function() {
-    const forecastImage = $('.card-image img');
-    console.log(forecastImage);
-    const iconcode = '01d';
-
     
-    // window.onresize = function(){
-    //     if (window.width <= 600) {
-    //         var iconurl = `http://openweathermap.org/img/w/${iconcode}.png`;
-    //         replaceImage(forecastImage);
-    //     } else if (window.width > 600) {
-    //         var iconurl = `http://openweathermap.org/img/w/${iconcode}@2x.png`;
-    //         replaceImage(forecastImage);
-    //     } else if (window.width > 992) {
-    //         var iconurl = `http://openweathermap.org/img/w/${iconcode}@4x.png`;
-    //         replaceImage(forecastImage);
-    //     }
-    // };
-    
-    function replaceImage(imgs) {
-        for (let i = 0; i < forecastImage.length; i++) {
-            let iconurl = forecastImage[i];
-            console.log(iconurl)
-        }
-    }
-
-    replaceImage()
 
     $('.sidenav').sidenav();
     $('.modal').modal();
